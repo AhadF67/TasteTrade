971009f24d519eabd0027e66de71984288b26cdb
bushraali98 <bm-bisho-90210@hotmail.com>
2024-08-20 11:21:06 +0300

    added home pages

--- TasteTrade/main/urls.py
+++ TasteTrade/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main_home, name='main_home'),
+    path('home/', views.main_home, name='main_home'),
     path('home-bus/', views.second_home, name='home_bus'),
     path('home-sup/', views.third_home, name='home_sup'),
 ]
--- TasteTrade/main/views.py
+++ TasteTrade/main/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.shortcuts import render
 
-def main_home(request):
+def main_home(request: HttpRequest):
     return render(request, 'main/main_home.html')
 
-def second_home(request):
+def second_home(request: HttpRequest):
     return render(request, 'main/home_bus.html')
 
-def third_home(request):
+def third_home(request: HttpRequest):
     return render(request, 'main/home_sup.html')
 
