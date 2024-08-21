0f414f575cf28a4af40e957899f4fe620f0227e8
Ghofran <ghofranalsanosi@gmail.com>
2024-08-21 14:05:10 +0300

    create Supplier profile

--- TasteTrade/accounts/forms.py
+++ TasteTrade/accounts/forms.py
@@ -4,12 +4,11 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
-    username = forms.CharField(max_length=150)
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['email', 'password']
         widgets = {
             'password': forms.PasswordInput(),
         }
