0b7792a59a74124a70f3b5117045ad597da447aa
bushraali98 <bm-bisho-90210@hotmail.com>
2024-08-26 11:28:33 +0300

    update generate pdf functionality

--- TasteTrade/accounts/urls.py
+++ TasteTrade/accounts/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('statics/', views.supplier_statistics, name='supplier_statistics'),
+    path('profile/pdf/', views.order_list_for_pdf, name='order_list_for_pdf'),
     
 ]
 
