f6bae3f61177449d3ba40e2f718cd3f1195a7dca
Ahad Fahad <ahadqw@hotmail.com>
2024-08-26 11:35:20 +0300

    //

--- TasteTrade/TasteTrade/settings.py
+++ TasteTrade/TasteTrade/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'BlueHuawei67_@outlook.com'
-EMAIL_HOST_PASSWORD = '12367Ahad'
+# EMAIL_HOST_USER = 'BlueHuawei67_@outlook.com'
+# EMAIL_HOST_PASSWORD = '12367Ahad'
