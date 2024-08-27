f757713a45e0c4a828ee2132d184e2df01577b1a
Noor Alresaini <nooralreseini@gmail.com>
2024-08-27 22:56:43 +0300

    footer links!

--- TasteTrade/TasteTrade/settings.py
+++ TasteTrade/TasteTrade/settings.py
@@ -145,10 +145,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+RECEIVE_APPLICATIONS_EMAIL = 'TasteTrade0@gmail.com'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'BlueHuawei67_@outlook.com'
-# EMAIL_HOST_PASSWORD = '12367Ahad'
+EMAIL_HOST_USER = 'BlueHuawei67_@outlook.com'
+EMAIL_HOST_PASSWORD = '12367Ahad'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- TasteTrade/main/urls.py
+++ TasteTrade/main/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
 from . import views
+from .views import submit_application
 
 urlpatterns = [
     path('', views.main_home, name='main_home'),
     path('home/', views.home, name='home'),
     path('meet-the-team/', views.meet_the_team, name='meet_the_team'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('our-services/', views.our_services, name='our_services'),
+    path('our-story/', views.our_story, name='our_story'),
+    path('sustainability/', views.sustainability, name='sustainability'),
+    path('careers/', views.careers, name='careers'),
+    path('faqs/', views.faqs, name='faqs'),
+    path('shipping-returns/', views.shipping_returns, name='shipping_returns'),
+    path('terms-of-service/', views.terms_of_service, name='terms_of_service'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
+    path('apply/', submit_application, name='submit_application'),
+    path('pricing/', views.pricing, name='pricing_page'),
+
 ]
